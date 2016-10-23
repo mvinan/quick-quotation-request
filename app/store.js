b1e3115bf928bcc965678f6b54f74c47c8242a50
Miguel Alejandro <admin@miguelvinan.com>
2016-10-23 13:42:13 -0500

    remove logger

--- app/store.js
+++ app/store.js
@@ -5,7 +5,7 @@ import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import promise from 'redux-promise-middleware'
 
-const middlewares = applyMiddleware(promise(), thunk, logger())
+const middlewares = applyMiddleware(promise(), thunk)
 const store = createStore(allReducers, middlewares)
 
 export default store
